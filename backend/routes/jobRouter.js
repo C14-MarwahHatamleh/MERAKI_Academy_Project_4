f7c2fe5b5951fd548228b8169e2996675bc3c6e7
MarwahHatamleh <marwahatamleh98@gmail.com>
2025-05-12 01:05:16 +0300

    updated the names of method and added new one for FilterBySalary

--- backend/routes/jobRouter.js
+++ backend/routes/jobRouter.js
@@ -5,11 +5,12 @@ const authzMiddlewares = require("../middleware/authz")
 const {
     createNewJob,
     getAllJobs,
-    getJobByTitle,
+    getJobBySearch,
     getJobById,
     updateJobById,
     deleteJobById,
     getJobByFilter,
+    getJobBySalary,
 
 } = require("../controllers/jobControllers");
 const jobRouter = express.Router();
@@ -17,8 +18,9 @@ const jobRouter = express.Router();
 jobRouter.post("/", authMiddlewares ,authzMiddlewares("CREATE_JOBS"), createNewJob);
 jobRouter.get("/", authMiddlewares ,getAllJobs);
 jobRouter.get("/byId/:id", getJobById);
-jobRouter.get("/search/:title", getJobByTitle);
+jobRouter.get("/search", getJobBySearch);
 jobRouter.get("/filter/:criteria", getJobByFilter);
+jobRouter.get("/filter/:min/:max", getJobBySalary);
 jobRouter.put("/update/:id",authMiddlewares ,authzMiddlewares("UPDATE_JOBS"), updateJobById);
 jobRouter.delete("/delete/:id",authMiddlewares ,authzMiddlewares("DELETE_JOBS"), deleteJobById);
 
