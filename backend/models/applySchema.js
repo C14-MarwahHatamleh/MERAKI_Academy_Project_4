fb8963f97a1621265549ec3ec8df516c9390b58f
MarwahHatamleh <marwahatamleh98@gmail.com>
2025-05-18 00:32:37 +0300

    added apply Schema

---
+++ backend/models/applySchema.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const ApplySchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  Phone: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  Education: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  salary: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  country: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  yearsOfExperiences: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  currentJob: {
+    type: String,
+    trim: true,
+  },
+  cvURL: {
+    type: String,
+    required: true,
+  },
+  applyDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+});
+
+ApplySchema.pre("save", async function () {
+  this.currentJob = this.currentJob.toLowerCase().trim();
+  this.Email = this.Email.toLowerCase().trim();
+  this.country = this.country.toLowerCase().trim();
+});
+
+const ApplyModel = mongoose.model("ApplyJob", ApplySchema);
+
+module.exports = ApplyModel;
