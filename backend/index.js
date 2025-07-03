0e195d0678b6c2b16ea8bcc4247751da9ff57c96
MarwahHatamleh <marwahatamleh98@gmail.com>
2025-07-03 15:03:48 +0300

    fix

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const jobRouter = require("../backend/routes/jobRouter");
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors())
